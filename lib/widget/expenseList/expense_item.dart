af8b148ea5b73506489412d4cbd557d3b036f332
anasmohammad611 <mohammadanas796@gmail.com>
2023-11-04 22:49:09 +0530

    expenses list and item view along with tool bar has been created

---
+++ lib/widget/expenseList/expense_item.dart
@@ -0,0 +1,40 @@
+import 'package:expense_tracker/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expense});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(
+              height: 8,
+            ),
+            Row(
+              children: [
+                Text(expense.amount.toStringAsFixed(2)),
+                const Spacer(),
+                Column(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(expense.formattedDate),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/expenseList/expenses_list.dart
@@ -0,0 +1,18 @@
+import 'package:expense_tracker/widget/expenseList/expense_item.dart';
+import 'package:flutter/material.dart';
+
+import '../../model/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => ExpenseItem(expense: expenses[index]),
+    );
+  }
+}
